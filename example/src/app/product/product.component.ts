2c8c3e988abc8b0668b2dc739161d00d72053935
tenkesnet <rbardo@gmail.com>
2024-04-27 14:04:25 +0200

    Signal mindenhol bevezetve

--- example/src/app/product/product.component.ts
+++ example/src/app/product/product.component.ts
@@ -1,32 +1,41 @@
-import { Component, computed, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
 import { Model } from '../repository.model';
+import { compilePipeFromMetadata } from '@angular/compiler';
 
 @Component({
   selector: 'app',
   templateUrl: './product.component.html',
-  styleUrl: './product.component.css'
+  styleUrl: './product.component.css',
+  //changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductComponent {
   private model: Model = new Model();
   private messages = ["Total", "Price"];
   private index = signal<number>(0);
 
-  get count(): number {
-    return this.model.getProducts().length;
-  }
+  // get count(): number {
+  //   console.log("Invoke count()");
+  //   return this.model.Products().length;
+  // }
+  count = computed(() => {
+    return this.model.Products().length;
+  });
+  countEffect = effect(() => console.log("Invoke count()"));
 
-  get total(): string {
-    return this.model.getProducts().
-      reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2);
-  }
+  // get total(): string {
+  //   console.log("Invoke total()");
+  //   return this.model.Products().
+  //     reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2);
+  // }
 
+  total = computed(() => this.model.Products().reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2));
 
   // get message(): string {
   //   return `${this.messages[this.index()]} $${this.total}`;
   // }
-  message = computed(() => {
-    return `${this.messages[this.index()]} $${this.total}`;
-  });
+  message = computed(() => `${this.messages[this.index()]} $${this.total()}`);
+  messageEffect = effect(() => console.log((`message value computed: ${this.message()}`)));
+
 
   toggleMessage(): void {
     //this.index = (this.index + 1) % 2;
@@ -34,6 +43,6 @@ export class ProductComponent {
   }
 
   removeProduct(): void {
-    this.model.deleteProduct(this.model.getProducts()[0]?.id ?? 0);
+    this.model.deleteProduct(this.model.Products()[0]?.id ?? 0);
   }
 }
--- example/src/app/repository.model.ts
+++ example/src/app/repository.model.ts
@@ -1,40 +1,42 @@
+import { Signal, WritableSignal, signal } from "@angular/core";
 import { SimpleDataSource } from "./SimpleDataSource.model";
 import { Product } from "./product.model";
 
 export class Model {
     private dataSource: SimpleDataSource;
-    private products: Product[];
+    private products: WritableSignal<Product[]>;
 
     constructor() {
         this.dataSource = new SimpleDataSource();
-        this.products = new Array<Product>();
-        this.dataSource.getData().forEach(p => { this.products.push(p) });
+        this.products = signal(new Array<Product>());
+        this.dataSource.getData().forEach(p => { this.products().push(p) });
+
     }
 
-    getProducts(): Product[] {
-        return this.products;
+    get Products(): Signal<Product[]> {
+        return this.products.asReadonly();
     }
 
     getProduct(id: number): Product | undefined {
-        return this.products.find(p => p.id == id);
+        return this.products().find(p => p.id == id);
     }
 
     saveProduct(product: Product): void {
         if (product.id == 0 || product.id == undefined) {
             product.id = this.generateId();
-            this.products.push(product);
+            this.products().push(product);
         } else {
-            let index = this.products.findIndex(p => p.id == product.id);
+            let index = this.products().findIndex(p => p.id == product.id);
             if (index > -1) {
-                this.products.splice(index, 1, product);
+                this.products().splice(index, 1, product);
             }
         }
     }
 
     deleteProduct(id: number): void {
-        let index = this.products.findIndex(p => p.id == id);
+        let index = this.products().findIndex(p => p.id == id);
         if (index > -1) {
-            this.products.splice(index, 1);
+            this.products.update(p => { p.splice(index, 1); return p.slice(0); });
         }
     }
 
